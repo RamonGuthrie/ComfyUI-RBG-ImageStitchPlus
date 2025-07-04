372626c7fb2e6823f9e97148a8939a7fe1d6c63f
Ramon Guthrie <156688666+RamonGuthrie@users.noreply.github.com>
2025-07-04 18:44:38 +0100

    Uploaded

---
+++ __init__.py
@@ -0,0 +1,38 @@
+from .nodes.ImageStitchAdvance import RBGImageStitchPlus
+
+NODE_CLASS_MAPPINGS = {
+    "RBGImageStitchPlus": RBGImageStitchPlus,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "RBGImageStitchPlus": "RBG Image Stitch Plus 🧩"
+}
+
+WEB_DIRECTORY = "./web"
+
+print("### Loading: ComfyUI-RBG-ImageStitchPlus ###")
+
+# Add Google Fonts Icons stylesheet
+import os
+import server
+
+# Get the directory of the current script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+js_path = os.path.join(dir_path, "web", "js")
+
+# Create the directory if it doesn't exist
+os.makedirs(js_path, exist_ok=True)
+
+# Create a new JS file to load the stylesheet
+js_file_path = os.path.join(js_path, "RBGSuitePack.js")
+with open(js_file_path, "w") as f:
+    f.write("""
+(function() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+    document.head.appendChild(link);
+})();
+""")
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
