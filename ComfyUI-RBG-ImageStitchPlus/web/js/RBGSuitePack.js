879d3c5397bf00e03f6ee7684016bbdd6a26a3ee
Ramon Guthrie <156688666+RamonGuthrie@users.noreply.github.com>
2025-07-04 18:43:57 +0100

    Delete ComfyUI-RBG-ImageStitchPlus directory

--- ComfyUI-RBG-ImageStitchPlus/web/js/RBGSuitePack.js
+++
@@ -1,7 +0,0 @@
-
-(function() {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
-    document.head.appendChild(link);
-})();
